4ac5e6164dcf08d2fed56f61502b54b418d08a11
aparnasuresh28 <84711961+aparnasuresh28@users.noreply.github.com>
2021-06-01 12:49:24 +0530

    Create wallnew.c

---
+++ wallnew.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+float wallis_pi(int);
+
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
+
+float wallis_pi(int k)
+{ float p,l,r,t;
+int j;
+for(j=0;j<k; j++)
+{l= (2 * j)/(2 * j- 1);
+        r = (2 * j)/(2 * j + 1);
+        t = l*r;
+        p+= 2*t;
+
+    return p;
+}}
